ea684af2c3ec2d54800f175194b46417c6461982
mdipaw <mdipawijaya@gmail.com>
2024-12-14 05:54:45 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,4 @@
 import {NextRequest, NextResponse} from 'next/server';
-import {jwtVerify} from "jose";
-
 export async function middleware(req: NextRequest) {
     if (req.nextUrl.pathname === '/login') {
         return NextResponse.next()
@@ -9,10 +7,7 @@ export async function middleware(req: NextRequest) {
     if (!session) {
         return NextResponse.redirect(new URL('/login', req.url));
     }
-    const data = await jwtVerify(session.value, new TextEncoder().encode(process.env.JWT_SECRET_KEY || 'secret')) as any
-    const response = NextResponse.next()
-    response.headers.set('x-user-data', JSON.stringify(data.payload));
-    return  response
+    return  NextResponse.next()
 }
 
 export const config = {
